34a72717da91efed979db2c8c2c15f1dbdeca829
xabgesagtx <xabgesagtx@riseup.net>
2018-05-03 08:51:15 +0200

    Repair cache invalidation

--- mensa-http/src/main/java/com/github/xabgesagtx/mensa/MensaHttpApplication.java
+++ mensa-http/src/main/java/com/github/xabgesagtx/mensa/MensaHttpApplication.java
@@ -2,11 +2,13 @@ package com.github.xabgesagtx.mensa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * Main class for the http application
  */
 @SpringBootApplication
+@EnableScheduling
 public class MensaHttpApplication {
 
 	public static void main(String[] args) {
